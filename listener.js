aef63d3f0f5db0bc62c62f077099aecb35eea826
Harichandra Prasath <1hcp302004@gmail.com>
2025-04-27 20:12:11 +0530

    .docx check, tab creation

--- listener.js
+++ listener.js
@@ -1,9 +1,48 @@
-function activeWatcher(delta) {
+function stateWatcher(delta) {
+
+	let id = delta.id;
 
 	if (delta.state && delta.state.current === "complete") {
-		console.log(delta.id);
+		browser.downloads.search({ id }).then(handleDownloaded);
 	}
 
 }
 
-browser.downloads.onChanged.addListener(activeWatcher)
+
+function handleDownloaded(downloadedFile) {
+
+	for (const download of downloadedFile) {
+
+		// Check for the docx extension
+		let fileName = download.filename;
+
+		if (fileName.endsWith(".docx")) {
+			console.log(`Got a .docx file with name ${fileName}`);
+			naviagateDocs()
+		} else {
+
+			console.log(`Recieved a non .docx file, Ignoring...`);
+
+		}
+	}
+}
+
+
+
+
+const naviagateDocs = () => {
+
+	browser.tabs.create({
+		url: "https://docs.google.com/",
+
+	}).then((tab) => {
+		console.log(`Created a new tab with id ${tab.id}`);
+	}, (err) => {
+		console.log(`Error in creating a new tab ${err}`);
+	})
+
+}
+
+
+
+browser.downloads.onChanged.addListener(stateWatcher)
