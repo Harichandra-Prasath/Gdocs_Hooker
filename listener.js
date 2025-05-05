03d1f9ff6b98e0dfbfb63de6295bceff4e8668c5
Harichandra Prasath <1hcp302004@gmail.com>
2025-05-05 10:39:22 +0530

    minor changes

--- listener.js
+++ listener.js
@@ -41,7 +41,7 @@ const startAction = async (fileName) => {
 
 
 	// Create a post method
-	const response = await fetch("http://127.0.0.1:8000/upload_to_drive", {
+	const response = await fetch(`http://127.0.0.1:${enginePort}/upload_to_drive`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
